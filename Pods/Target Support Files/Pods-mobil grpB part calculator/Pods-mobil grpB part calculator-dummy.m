35810b0ea2dfa9eb3c7c51da5b8c1e808ecf73eb
Mohammad Hassan <mohammad.hassan@tuni.fi>
2020-10-19 22:16:01 +0300

    zip file removed

---
+++ Pods/Target Support Files/Pods-mobil grpB part calculator/Pods-mobil grpB part calculator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mobil_grpB_part_calculator : NSObject
+@end
+@implementation PodsDummy_Pods_mobil_grpB_part_calculator
+@end
---
+++ Pods/Target Support Files/Pods-mobil grpB part calculatorTests/Pods-mobil grpB part calculatorTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mobil_grpB_part_calculatorTests : NSObject
+@end
+@implementation PodsDummy_Pods_mobil_grpB_part_calculatorTests
+@end
